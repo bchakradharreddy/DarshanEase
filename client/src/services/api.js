86c8f71107bafb4941ac7428603dab4a4935098d
bchakradharreddy <bchakradhar999@gmail.com>
2026-07-04 10:36:42 +0530

    Change API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://darshanease-backend-p2jc.onrender.com",
 });
 
 // Add JWT token automatically
